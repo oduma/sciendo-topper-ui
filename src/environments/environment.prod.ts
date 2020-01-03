67828be8ba6e8ea6b3366e998fa16ec73175207f
oduma <tavi_duma2002@yahoo.co.uk>
2020-01-03 18:58:37 +0000

    Trying to fix the cache-busting

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
-  urlAddress: "http://192.168.1.4:8888"
+  urlAddress: "http://127.0.0.1:8888",
+  lastFmUrlAddress:"http://ws.audioscrobbler.com/2.0/"
 
 };
