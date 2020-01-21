833ccdf42d0ee6effc772ecebd7bdb611e676fbf
oduma <tavi_duma2002@yahoo.co.uk>
2020-01-21 08:18:58 +0000

    configured the qa as an environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,9 +4,9 @@
 
 export const environment = {
   production: false,
-  urlAddress: "http://127.0.0.1:8888",
-  lastFmUrlAddress:"http://ws.audioscrobbler.com/2.0/"
-  //urlAddress: "http://localhost:35799"
+  //urlAddress: "http://127.0.0.1:8888",
+  lastFmUrlAddress:"http://ws.audioscrobbler.com/2.0/",
+  urlAddress: "http://localhost:35799"
 };
 
 /*
