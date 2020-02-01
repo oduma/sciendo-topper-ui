bf0b18e7c57fa3ff2eecbe61d5e39e6e6a259e62
oduma <tavi_duma2002@yahoo.co.uk>
2020-02-01 13:59:32 +0000

    fixed the env prod url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  urlAddress: "http://127.0.0.1:8888",
+  urlAddress: "http://media-centre:8888",
   lastFmUrlAddress:"http://ws.audioscrobbler.com/2.0/"
 
 };
